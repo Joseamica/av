b7bd408dc8a1312ba043f95896a5dc12bf94b1b5
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-07-07 16:04:11 -0600

    ONTESTING if data, then use revalidator, if not then do not revalidate

--- app/use-live-loader.tsx
+++ app/use-live-loader.tsx
@@ -10,18 +10,21 @@ import {useEventSource} from 'remix-utils'
 export function useLiveLoader<T>() {
   const eventName = useLocation().pathname
   const data = useEventSource(`/events${eventName}`)
+
   const navigate = useNavigate()
   const {revalidate} = useRevalidator()
 
   // FIXME- esto hace que cuando se hace save en vscode, se recarga la pagina multiples veces
   useEffect(() => {
-    switch (data) {
-      case 'endOrder':
-        navigate('processes/endOrder')
-        break
-    }
+    if (data) {
+      switch (data) {
+        case 'endOrder':
+          navigate('processes/endOrder')
+          break
+      }
 
-    revalidate()
+      revalidate()
+    }
   }, [data, revalidate])
 
   return useLoaderData<T>()
