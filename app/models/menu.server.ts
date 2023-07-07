a424fc716d8f7440954d17a43e6466cf900b14d3
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-07-07 14:06:14 -0600

    modify menu fetch on menu.sever.ts

--- app/models/menu.server.ts
+++ app/models/menu.server.ts
@@ -1,8 +1,51 @@
 import {prisma} from '~/db.server'
 import {getHour} from '~/utils'
 
+// export function getMenu(branchId: string | undefined) {
+//   const timeNow = getHour()
+
+//   return prisma.menu.findFirst({
+//     where: {
+//       branchId,
+//       OR: [
+//         {
+//           allday: true,
+//         },
+//         {
+//           AND: [
+//             {
+//               fromHour: {
+//                 lte: timeNow,
+//               },
+//             },
+//             {
+//               toHour: {
+//                 gte: timeNow,
+//               },
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   })
+// }
+
+function getDayOfWeek() {
+  const date = new Date()
+  let day = date.getDay()
+
+  // Adjust day number to match your database format
+  if (day === 0) {
+    // if it's Sunday
+    day = 7 // make it 7
+  }
+
+  return day
+}
+
 export function getMenu(branchId: string | undefined) {
   const timeNow = getHour()
+  const dayOfWeekNow = getDayOfWeek()
 
   return prisma.menu.findFirst({
     where: {
@@ -23,9 +66,25 @@ export function getMenu(branchId: string | undefined) {
                 gte: timeNow,
               },
             },
+            {
+              availabilities: {
+                some: {
+                  dayOfWeek: dayOfWeekNow,
+                  startTime: {
+                    lte: String(timeNow),
+                  },
+                  endTime: {
+                    gte: String(timeNow),
+                  },
+                },
+              },
+            },
           ],
         },
       ],
+    },
+    include: {
+      availabilities: true,
     },
   })
 }
