322cda9f79418a08639167d0bd70ef328e9805e2
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-07-07 13:59:22 -0600

    added deliverect table sync

--- app/routes/api.dvct.register.tsx
+++ app/routes/api.dvct.register.tsx
@@ -1,18 +1,9 @@
 import {json, redirect} from '@remix-run/node'
-import fetch from 'node-fetch'
+import type {ActionArgs} from '@remix-run/node'
 
 export const action = async ({request}: ActionArgs) => {
-  //   throw new Error("GRAPHQL_API is required");
-  // }
   const rawData = await request.text()
   const data = JSON.parse(rawData)
 
-  // const body: any = { query };
-
-  // if (variables) body.variables = variables;
   return json({success: true})
 }
-
-// export default function apI() {
-//   return <div>api</div>
-// }
---
+++ app/routes/api.dvct.tables.$locationId.tsx
@@ -0,0 +1,62 @@
+import {json} from '@remix-run/node'
+import type {LoaderArgs} from '@remix-run/node'
+import cuid from 'cuid'
+
+import {prisma} from '~/db.server'
+import {getTableIdFromUrl} from '~/utils'
+
+export async function loader({request, params}: LoaderArgs) {
+  const {locationId} = params
+
+  const token = await prisma.deliverect
+    .findFirst({})
+    .then(res => res?.deliverectToken)
+  const pathname = new URL(request.url).pathname
+  const tableId = getTableIdFromUrl(pathname)
+
+  const url = 'https://api.staging.deliverect.com/tables/' + locationId
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Bearer ' + token,
+    },
+  }
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log('data', data)
+    const tableData = data.tables
+
+    for (const table of tableData) {
+      const tableNumber = Number(table.id.replace(/\D/g, ''))
+
+      await prisma.table.upsert({
+        where: {id: table.id},
+        update: {
+          table_number: tableNumber,
+          floorId: table.floorId,
+          seats: table.seats,
+          locationId: locationId,
+          branchId: locationId,
+        },
+        create: {
+          id: cuid(),
+          // id: table.id,
+          table_number: tableNumber,
+          floorId: table.floorId,
+          seats: table.seats,
+          locationId: locationId,
+          order_in_progress: false,
+          branchId: locationId,
+        },
+      })
+    }
+
+    // return redirect(`/table/${tableId}`, {status: 303})
+  } catch (err) {
+    console.error('error:' + err)
+    return json({tokenAssign: false}) // Or throw an error, or return some other value indicating the request failed.
+  }
+}
+//
