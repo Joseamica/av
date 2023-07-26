dcdbcca98a8b104582d33eed61d242ffadd1b4a1
Carlos Vera <kikiondo117@gmail.com>
2023-07-25 19:32:34 -0600

    kikis-01 refactor

--- app/routes/api.dvct.oauth.token.tsx
+++ app/routes/api.dvct.oauth.token.tsx
@@ -1,70 +1,7 @@
-import {type ActionArgs, type LoaderArgs, json, redirect} from '@remix-run/node'
+import {type ActionArgs, json, redirect} from '@remix-run/node'
 import invariant from 'tiny-invariant'
 import {prisma} from '~/db.server'
 import {getSession} from '~/session.server'
-
-export async function loader({request, params}: LoaderArgs) {
-  const rawData = await request.text()
-
-  // Parse the raw data to JSON
-  //   const data = JSON.parse(rawData)
-  //   console.log(data)
-
-  return json({success: true})
-}
-
-// export async function action({request, params}: ActionArgs) {
-//   const clientId = process.env.DELIVERECT_CLIENT_ID
-//   const secret = process.env.DELIVERECT_SECRET
-//   const session = await getSession(request)
-//   const tableId = session.get('tableId')
-
-//   const token = await getToken(request)
-//   const table = await prisma.table.findFirst({
-//     where: {id: tableId},
-//     select: {deliverectToken: true},
-//   })
-//   const isDeliverectToken = table?.deliverectToken ? true : false
-
-//   if (!token && !isDeliverectToken) {
-//     const url = 'https://api.staging.deliverect.com/oauth/token'
-//     const options = {
-//       method: 'POST',
-//       headers: {accept: 'application/json', 'content-type': 'application/json'},
-//       body: JSON.stringify({
-//         audience: 'https://api.staging.deliverect.com',
-//         grant_type: 'token',
-//         client_id: clientId,
-//         client_secret: secret,
-//       }),
-//     }
-//     try {
-//       const response = await fetch(url, options)
-//       const data = await response.json()
-
-//       await setToken(session, data.access_token)
-//       await prisma.table.update({
-//         where: {id: tableId},
-//         data: {deliverectToken: data.access_token},
-//       })
-
-//       return redirect(`/table/${tableId}`, {
-//         headers: {
-//           'Set-Cookie': await sessionStorage.commitSession(session, {
-//             expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-//             maxAge: 1000 * 60 * 60 * 24,
-//             secure: true,
-//           }),
-//         },
-//       })
-//     } catch (err) {
-//       console.error('error:' + err)
-//       return json({success: false}) // Or throw an error, or return some other value indicating the request failed.
-//     }
-//   }
-
-//   return redirect(`/table/${tableId}`)
-// }
 
 export async function action({request, params}: ActionArgs) {
   const clientId = process.env.DELIVERECT_CLIENT_ID
@@ -74,6 +11,7 @@ export async function action({request, params}: ActionArgs) {
 
   const deliverect = await prisma.deliverect.findFirst({})
   invariant(deliverect, 'deliverect not found')
+
   const url = `${process.env.DELIVERECT_API_URL}/oauth/token`
   const options = {
     method: 'POST',
@@ -85,6 +23,7 @@ export async function action({request, params}: ActionArgs) {
       client_secret: secret,
     }),
   }
+
   try {
     const response = await fetch(url, options)
     const data = await response.json()
