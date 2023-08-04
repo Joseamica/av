187d672e7060cc0a72240d044781f728fef87e42
Carlos Vera <kikiondo117@gmail.com>
2023-08-03 19:42:51 -0600

    TAS-31 TIP MODAL UI

--- app/components/payment/payment.tsx
+++ app/components/payment/payment.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import clsx from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 
+import { ChevronRightIcon, ChevronUpIcon } from '..'
 import { SubModal } from '../modal'
 
 import { Translate, formatCurrency } from '~/utils'
@@ -12,7 +13,7 @@ import { Button } from '~/components/ui/buttons/button'
 import { FlexRow } from '~/components/util/flexrow'
 import { Spacer } from '~/components/util/spacer'
 import { H2, H3, H4, H5, H6 } from '~/components/util/typography'
-import { ChevronRightIcon, ChevronUpIcon } from '..'
+import { Underline } from '~/components/util/underline'
 
 const variants = {
   hidden: {
@@ -115,6 +116,7 @@ export function PaymentV2({
                 </FlexRow>
               </button>
               <Spacer spaceY="2" />
+              {/* ANCHOR TIP */}
               <button className="flex flex-row items-center justify-between" type="button" onClick={() => setShowModal({ ...showModal, payment: true })}>
                 <H5>Método de pago</H5>
                 <FlexRow>
@@ -162,15 +164,48 @@ export function PaymentV2({
       </div>
 
       {showModal.tip && (
-        <AssignTipModal
-          amountToPay={amountToPayState}
-          currency={currency}
-          handleTipChange={handleTipChange}
-          setShowModal={setShowModal}
-          showModal={showModal}
-          tipPercentages={tipsPercentages}
-          tipRadio={tipRadio}
-        />
+        <SubModal onClose={() => setShowModal({ ...showModal, tip: false })} title="Asignar propina">
+          <div className="flex flex-col space-y-2">
+            {tipsPercentages &&
+              tipsPercentages.map((tipPercentage: any) => (
+                <label
+                  key={tipPercentage}
+                  className={clsx(
+                    'flex w-full flex-row items-center justify-center space-x-2 rounded-lg border border-button-outline border-opacity-40 px-3 py-1 text-center shadow-lg',
+                    {
+                      'text-2 rounded-full bg-button-primary px-2 py-1  text-white  ring-4   ring-button-outline': tipRadio.toString() === tipPercentage,
+                    },
+                  )}
+                >
+                  <FlexRow justify="between" className="w-full">
+                    <H4>
+                      {tipPercentage >= 10 && tipPercentage < 12
+                        ? 'Muchas gracias!'
+                        : tipPercentage >= 12 && tipPercentage < 15
+                        ? 'Excelente servicio!'
+                        : tipPercentage >= 15 && tipPercentage < 18
+                        ? '❤️ Wow!'
+                        : tipPercentage >= 18
+                        ? 'Eres increible!'
+                        : tipPercentage === '0'
+                        ? 'No dejar propina'
+                        : 'otro'}
+                    </H4>
+                    <H3>{tipPercentage}%</H3>
+                  </FlexRow>
+                  <input type="radio" name="tipPercentage" value={tipPercentage} onChange={handleTipChange} className="sr-only" />
+                </label>
+              ))}
+          </div>
+          <Spacer spaceY="2" />
+          <H3 className="flex w-full flex-row justify-center">
+            <Underline>Estas dejando {formatCurrency(currency, (tipRadio * total) / 100)} de propina</Underline>
+          </H3>
+          <Spacer spaceY="2" />
+          <Button fullWith={true} onClick={() => setShowModal({ ...showModal, tip: false })}>
+            Asignar
+          </Button>
+        </SubModal>
       )}
 
       {showModal.payment && (
