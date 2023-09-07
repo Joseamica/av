2f0fe654465802159b82d29b06a311fecf8d5ba3
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-09-07 17:46:38 -0600

    changes

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,9 +1,11 @@
 import { Form, Link, useLoaderData } from '@remix-run/react'
+import React from 'react'
 import { FaEdit } from 'react-icons/fa'
 
 import { type ActionArgs, type LoaderArgs, json, redirect } from '@remix-run/node'
 
 import type { Chain } from '@prisma/client'
+import invariant from 'tiny-invariant'
 import { prisma } from '~/db.server'
 import { getSession } from '~/session.server'
 
@@ -11,8 +13,14 @@ import { Button, H2 } from '~/components'
 import { EditRestDialog } from '~/components/admin/ui/dialogs/edit-rest-dialog'
 import SelectBranchDialog from '~/components/admin/ui/dialogs/select-branch-dialog'
 
+/**
+ * Fetches data from the server and returns it to the component.
+ * It retrieves the user's session, checks their roles, and fetches chains and selected chain data based on the user's roles and query parameters.
+ *
+ * @param {object} request - The request object containing information about the HTTP request.
+ * @returns {object} - JSON object containing the fetched chains and selected chain data.
+ */
 export const loader = async ({ request }: LoaderArgs) => {
-  // const admin = await requireAdmin(request)
   const session = await getSession(request)
   const userId = session.get('userId')
 
@@ -27,37 +35,26 @@ export const loader = async ({ request }: LoaderArgs) => {
     },
   })
   if (!user || user.roles.length === 0) {
-    throw json({ error: 'Unauthorized', requiredRole: 'admin' }, { status: 403 })
+    return json({ error: 'User does not have any roles assigned' }, { status: 403 })
   }
-  const roles = user?.roles.map(role => role.name)
+  const roles = user.roles.map(role => role.name)
 
   let chains = null
+  let whereClause = {}
   if (roles.includes('moderator')) {
-    chains = await prisma.chain.findMany({
-      where: {
-        moderatorIds: {
-          has: userId,
-        },
+    whereClause = {
+      moderatorIds: {
+        has: userId,
       },
-      include: {
-        branches: true,
-      },
-    })
+    }
   }
 
-  if (roles.includes('admin')) {
-    chains = await prisma.chain.findMany({
-      include: {
-        branches: true,
-      },
-    })
-  }
-
-  // const chains = await prisma.chain.findMany({
-  //   where: {
-  //     adminId: admin.adminId,
-  //   },
-  // })
+  chains = await prisma.chain.findMany({
+    where: whereClause,
+    include: {
+      branches: true,
+    },
+  })
 
   const searchParams = new URL(request.url).searchParams
   const chainId = searchParams.get('chainId') || null
@@ -79,20 +76,30 @@ export const loader = async ({ request }: LoaderArgs) => {
 export async function action({ request, params }: ActionArgs) {
   const formData = await request.formData()
   const branchId = formData.get('branchId')
+  invariant(branchId, 'branchId is required')
 
-  return redirect(`${branchId}`)
+  const redirectUrl = branchId ? `${branchId}` : ''
+  return redirect(redirectUrl)
 }
 
+/**
+ * Renders the admin dashboard component.
+ * It displays a list of chains and allows the user to edit and select branches within each chain.
+ *
+ * @returns The rendered admin dashboard component.
+ */
 export default function Admin() {
   const data = useLoaderData()
   // TODO - on loader, it will get user role and depending on the user role, we will return specific component
   // For example: admin, all admin dashboard (avoqado devs), moderator (owner of the restaurant) all the branches assigned.
-  const isBranches = data.selectedChain?.branches.length >= 1
+  const isBranches = Array.isArray(data.selectedChain?.branches) && data.selectedChain?.branches.length > 0
+  const MemoizedEditRestDialog = React.memo(EditRestDialog)
+  const MemoizedSelectBranchDialog = React.memo(SelectBranchDialog)
 
   return (
     <main className="m-auto min-h-screen flex flex-col">
-      <EditRestDialog />
-      <SelectBranchDialog selectedChain={data.selectedChain} isBranches={isBranches} />
+      <MemoizedEditRestDialog />
+      <MemoizedSelectBranchDialog selectedChain={data.selectedChain} isBranches={isBranches} />
 
       <div className="fixed top-20 w-full flex justify-between">
         <H2 className="">Avoqado</H2>
@@ -102,25 +109,25 @@ export default function Admin() {
       </div>
 
       <div className="flex flex-grow justify-center items-center gap-2">
-        {data.chains.map((chain: Chain) => (
-          <div className="flex flex-col space-y-4 items-center" key={chain.id}>
-            <Link
-              to={`?chainId=${chain.id}`}
-              // onClick={() => setShowModal({ editRest: false, selectBranch: !showModal.selectBranch })}
-              className="w-52 h-52 flex-shrink flex justify-center items-center bg-white shadow-lg rounded-xl"
-            >
-              {chain.name}
-            </Link>
+        {Array.isArray(data.chains) &&
+          data.chains.map((chain: Chain) => (
+            <div className="flex flex-col space-y-4 items-center" key={chain.id}>
+              <Link
+                to={`?chainId=${chain.id}`}
+                className="w-52 h-52 flex-shrink flex justify-center items-center bg-white shadow-lg rounded-xl"
+              >
+                {chain.name}
+              </Link>
 
-            <Link
-              to={`?editChain=${chain.id}`}
-              className=" flex-row space-x-2 text-violet11 shadow-blackA7 hover:bg-mauve3 flex h-[35px] items-center justify-center rounded-full bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
-            >
-              <FaEdit />
-              <p>Edit</p>
-            </Link>
-          </div>
-        ))}
+              <Link
+                to={`?editChain=${chain.id}`}
+                className=" flex-row space-x-2 text-violet11 shadow-blackA7 hover:bg-mauve3 flex h-[35px] items-center justify-center rounded-full bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
+              >
+                <FaEdit />
+                <p>Edit</p>
+              </Link>
+            </div>
+          ))}
       </div>
       <Form method="POST" action="/logout">
         <Button type="submit">Logout</Button>
