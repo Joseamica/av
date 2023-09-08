894bb6b96ebdbb27d80dc398bde096d094aef83f
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-09-07 19:04:25 -0600

    step point

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,5 +1,4 @@
 import { Form, Link, useLoaderData } from '@remix-run/react'
-import React from 'react'
 import { FaEdit } from 'react-icons/fa'
 
 import { type ActionArgs, type LoaderArgs, json, redirect } from '@remix-run/node'
@@ -13,14 +12,8 @@ import { Button, H2 } from '~/components'
 import { EditRestDialog } from '~/components/admin/ui/dialogs/edit-rest-dialog'
 import SelectBranchDialog from '~/components/admin/ui/dialogs/select-branch-dialog'
 
-/**
- * Fetches data from the server and returns it to the component.
- * It retrieves the user's session, checks their roles, and fetches chains and selected chain data based on the user's roles and query parameters.
- *
- * @param {object} request - The request object containing information about the HTTP request.
- * @returns {object} - JSON object containing the fetched chains and selected chain data.
- */
 export const loader = async ({ request }: LoaderArgs) => {
+  // const admin = await requireAdmin(request)
   const session = await getSession(request)
   const userId = session.get('userId')
 
@@ -82,24 +75,16 @@ export async function action({ request, params }: ActionArgs) {
   return redirect(redirectUrl)
 }
 
-/**
- * Renders the admin dashboard component.
- * It displays a list of chains and allows the user to edit and select branches within each chain.
- *
- * @returns The rendered admin dashboard component.
- */
 export default function Admin() {
   const data = useLoaderData()
   // TODO - on loader, it will get user role and depending on the user role, we will return specific component
   // For example: admin, all admin dashboard (avoqado devs), moderator (owner of the restaurant) all the branches assigned.
-  const isBranches = Array.isArray(data.selectedChain?.branches) && data.selectedChain?.branches.length > 0
-  const MemoizedEditRestDialog = React.memo(EditRestDialog)
-  const MemoizedSelectBranchDialog = React.memo(SelectBranchDialog)
+  const isBranches = data.selectedChain?.branches.length >= 1
 
   return (
     <main className="m-auto min-h-screen flex flex-col">
-      <MemoizedEditRestDialog />
-      <MemoizedSelectBranchDialog selectedChain={data.selectedChain} isBranches={isBranches} />
+      <EditRestDialog />
+      <SelectBranchDialog selectedChain={data.selectedChain} isBranches={isBranches} />
 
       <div className="fixed top-20 w-full flex justify-between">
         <H2 className="">Avoqado</H2>
@@ -109,25 +94,25 @@ export default function Admin() {
       </div>
 
       <div className="flex flex-grow justify-center items-center gap-2">
-        {Array.isArray(data.chains) &&
-          data.chains.map((chain: Chain) => (
-            <div className="flex flex-col space-y-4 items-center" key={chain.id}>
-              <Link
-                to={`?chainId=${chain.id}`}
-                className="w-52 h-52 flex-shrink flex justify-center items-center bg-white shadow-lg rounded-xl"
-              >
-                {chain.name}
-              </Link>
+        {data.chains.map((chain: Chain) => (
+          <div className="flex flex-col space-y-4 items-center" key={chain.id}>
+            <Link
+              to={`?chainId=${chain.id}`}
+              // onClick={() => setShowModal({ editRest: false, selectBranch: !showModal.selectBranch })}
+              className="w-52 h-52 flex-shrink flex justify-center items-center bg-white shadow-lg rounded-xl"
+            >
+              {chain.name}
+            </Link>
 
-              <Link
-                to={`?editChain=${chain.id}`}
-                className=" flex-row space-x-2 text-violet11 shadow-blackA7 hover:bg-mauve3 flex h-[35px] items-center justify-center rounded-full bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
-              >
-                <FaEdit />
-                <p>Edit</p>
-              </Link>
-            </div>
-          ))}
+            <Link
+              to={`?editChain=${chain.id}`}
+              className=" flex-row space-x-2 text-violet11 shadow-blackA7 hover:bg-mauve3 flex h-[35px] items-center justify-center rounded-full bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
+            >
+              <FaEdit />
+              <p>Edit</p>
+            </Link>
+          </div>
+        ))}
       </div>
       <Form method="POST" action="/logout">
         <Button type="submit">Logout</Button>
