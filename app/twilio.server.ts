89fb282533b9cbf5a8579f6c288a56af67a08327
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2023-10-18 12:29:14 -0600

    added sms

--- app/twilio.server.ts
+++ app/twilio.server.ts
@@ -24,11 +24,11 @@ export async function sendWaNotification({ from = '14155238886', to, body }: { f
     return client.messages
       .create({
         body,
-        from: `whatsapp:${from}`,
-        to: `whatsapp:+${phone}`,
+        // from: `whatsapp:${from}`,
+        // to: `whatsapp:+${phone}`,
         // body: 'Test',
-        // from: '+18149149288',
-        // to: '+525512956265',
+        from: '+18149149288',
+        to: '+' + phone,
       })
       .catch(error => {
         console.error(`Error sending WhatsApp message to +${phone}:`, error)
